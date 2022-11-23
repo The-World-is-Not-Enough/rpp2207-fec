31b61d57f3c02072ac4856a8636fea45e059ad55
Calvin Goodale <calvin.goodale@gmail.com>
2022-11-22 20:17:53 -0500

    Fixed small bug (needed to add more detailed ID to LinearGradient)

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -20,46 +20,77 @@ const App = () => {
     setYourOutfit(yourOutfit => ([...yourOutfit, product]));
   }
 
-  const fullStar = (id) => {
+  const fullStar = (id, st, key) => {
+    if (st === 'f') {
+      return (
+        <svg key={`${key}-${id}`} width="11px" height="11px" viewBox="0 0 31 31">
+          <defs>
+            <linearGradient id="grad-full">
+              <stop offset="100%" stopColor="black"/>
+            </linearGradient>
+          </defs>
+          <path fill="url(#grad-full)" stroke="black" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118
+        l11.547-1.2L16.026,0.6L20.388,10.918z"/>
+        </svg>
+      )
+    } else if (st === 'e') {
+      return (
+        <svg key={`${key}-${id}`} width="11px" height="11px" viewBox="0 0 31 31">
+          <defs>
+            <linearGradient id="grad-full">
+              <stop offset="100%" stopColor="white"/>
+            </linearGradient>
+          </defs>
+          <path fill="white" stroke="black" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118
+        l11.547-1.2L16.026,0.6L20.388,10.918z"/>
+        </svg>
+      )
+    }
+
+  }
+
+  const halfStar = (id, key, amount) => {
     return (
-      <svg key={`star-${id}`} width="11px" height="11px" viewBox="0 0 31 31">
+      <svg key={`${key}-${id}`} width="11px" height="11px" viewBox="0 0 31 31">
         <defs>
-          <linearGradient id="grad-full">
-            <stop offset="100%" stopColor="black"/>
+          <linearGradient id={`grad-${key}-${amount}-${id}`}>
+          <stop offset={`${amount}%`} stopColor="black" stopOpacity='1'/>
+          <stop offset={`${amount}%`} stopColor="white" stopOpacity='1'/>
           </linearGradient>
         </defs>
-        <path fill="url(#grad-full)" stroke="black" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118
+        <path fill={`url(#grad-${key}-${amount}-${id})`} stroke="black" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118
       l11.547-1.2L16.026,0.6L20.388,10.918z"/>
       </svg>
     )
   }
 
-  const halfStar = (id) => {
-    return (
-      <svg key={`star-${id}`} width="11px" height="11px" viewBox="0 0 31 31">
-        <defs>
-          <linearGradient id="grad-half">
-          <stop offset="50%" stopColor="black"/>
-            <stop offset="50%" stopColor="white"/>
-          </linearGradient>
-        </defs>
-        <path fill="url(#grad-half)" stroke="black" d="M20.388,10.918L32,12.118l-8.735,7.749L25.914,31.4l-9.893-6.088L6.127,31.4l2.695-11.533L0,12.118
-      l11.547-1.2L16.026,0.6L20.388,10.918z"/>
-      </svg>
-    )
-  }
-
-  const generateStars = (rating) => {
+  const generateStars = (rating, key) => {
+    console.log(rating, key)
     const stars = [];
     const fullStars = Math.floor(rating);
-    var i = 0;
-    for (i; i < fullStars; i++) {
-      stars.push(fullStar(i));
+    for (let i = 0; i < 5; i++) {
+      if (i === fullStars) {
+        let values = [0, 0.25, 0.5, 0.75, 1];
+        let roundedStarVal = values.map((val) => {
+          return Math.abs(val - (rating % 1));
+        })
+        if (rating % 1 > 0.0001) {
+          let partialStar = values[roundedStarVal.indexOf(Math.min(...roundedStarVal))] * 100;
+          console.log('partialStar: ', partialStar)
+          stars.push(halfStar(i, key, partialStar));
+        } else {
+          stars.push(fullStar(i, 'e', key));
+        }
+        continue;
+      }
+
+      if (i < fullStars) {
+        stars.push(fullStar(i, 'f', key));
+      } else if ( i > fullStars - 1) {
+        stars.push(fullStar(i, 'e', key))
+      }
     }
-    if (rating.toString().includes('.5')) {
-      i ++;
-      stars.push(halfStar(i));
-    }
+    console.log(stars)
     return stars;
   }
 
